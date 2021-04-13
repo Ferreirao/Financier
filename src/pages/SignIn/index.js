57b8c085a4ea062561dcf493f6a48069152b4a57
Savio Ferreira <saviio.ferreiira15@gmail.com>
2021-04-12 22:22:39 -0300

    esconder senha e loading nas requisições

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import { Platform } from 'react-native';
+import { Platform, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { AuthContext } from '../../contexts/auth';
 
@@ -12,7 +12,7 @@ export default function SignIn() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { signIn } = useContext(AuthContext);
+  const { signIn, loadingAuth } = useContext(AuthContext);
 
 
   function handleLogin(){
@@ -43,12 +43,21 @@ export default function SignIn() {
           autoCorrect={false}
           autoCapitalize="none"
           value={password}
+          caretHidden={false}
           onChangeText={ (text) => setPassword(text) }
+          secureTextEntry={true}
           />
         </AreaInput>
 
       <SubmitButton onPress={handleLogin}>
-        <SubmitText>Acessar</SubmitText>
+        { loadingAuth ? 
+        (
+          <ActivityIndicator size={20} color="#fff" />
+        ) : (
+          <SubmitText>Acessar</SubmitText>
+        )
+        }
+        
       </SubmitButton>
 
       <Link onPress={ () => navigation.navigate('SignUp')}>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import { Platform } from 'react-native';
+import { Platform, ActivityIndicator } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth';
 
@@ -11,7 +11,7 @@ export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   
-  const { signUp } = useContext(AuthContext);
+  const { signUp, loadingAuth } = useContext(AuthContext);
 
   function handleSignUp(){
     signUp(email, password, nome);
@@ -51,11 +51,18 @@ export default function SignIn() {
           autoCapitalize="none"
           value={password}
           onChangeText={ (text) => setPassword(text) }
+          secureTextEntry={true}
           />
         </AreaInput>
 
       <SubmitButton onPress={handleSignUp}>
-        <SubmitText>Cadastrar</SubmitText>
+      { loadingAuth ? 
+        (
+          <ActivityIndicator size={20} color="#fff" />
+        ) : (
+          <SubmitText>Cadastrar</SubmitText>
+        )
+        }
       </SubmitButton>
 
       </Container>
